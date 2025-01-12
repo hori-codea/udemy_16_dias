0f2c879293ae7e3391a9a05c52f96d8503fb7bc7
Hori <horaciomiras@gmail.com>
2025-01-11 22:19:45 -0300

    Tipos de datos

--- anotaciones.py
+++ anotaciones.py
@@ -3,3 +3,42 @@ print('Hello world! Testing the print function')
 print(2+2)
 
 print('hola'+" "+'hola')
+
+# Tipos de datos:
+
+# Los datos es información básicamente, que pueden ser de distintos tipos. Los más comunes en programación serían los siguientes:
+
+# String (str) --> Texto --> Siempre todo lo que esté entre '' o ""
+nombre = 'pepe'
+
+# Integer (int) --> Números ENTEROS
+
+positivo = 1
+negativo = -1
+
+# Floating (float) --> Números DECIMALES
+positivo = 1.1
+negativo = -0.5
+
+# Listas (list) --> colección ORDENADA de objetos --> se escriben entre [] y pueden contener objetos de todo tipo
+    # Algo importante de las listas es el tema del ORDEN. Cada elemento tiene un lugar asignado (índece)
+    # Las listas SIEMPRE empiezan con el índice 0 (de izquiera a derecha) o -1 (de derecha a izquierda)
+ejemplo = ['Juan', 'Pablo', 1, 'Carlos', 10.50]
+
+# Diccionarios (dic) --> Consisten en pares de palabras agrupados. Se escriben entre {}.
+    # Los diccionarios contienen lo que son claves y valores. Ejemplo: clave: 'color' ; valor 'rojo'
+    # En los diccionarios los pares no están ordenados con un índice.
+dic_ejemplo = {'color':'rojo', 'música':'rock'}
+
+# Tuples (tuple) --> Se escriben entre () y son muy parecidos a las listas, ya que son un conjunto ORDENADO de elementos
+# PERO la diferencia con las listas es que el orden es INMUTABLE. En las listas hay métodos que permiten agregar, reordenar, quitar, etc.
+tupla = ('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo')
+
+# Sets (set) --> Se escriben entre {} --> Es un conjunto ORDENADO de elementos UNICOS.
+# Tanto las listas como las tuplas pueden tener elementos repetidos. Los sets, en cambio, tienen elementos UNICOS e IRREPETIBLES.
+set_ejemplo = {'a', 'b', 'c'}
+
+# Booleanos (bool) --> El booleano es un dato que puede tener sólo 2 valores (True o False).
+
+
+
